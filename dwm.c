dc460b0e2c58f2aaabcc6153aa460f09fcdc82e8
Federico Ciuffardi <federico.ciuffardi@outlook.com>
2020-07-24 18:52:02 -0300

    zoom follows focus

--- dwm.c
+++ dwm.c
@@ -3120,6 +3120,7 @@ void
 zoom(const Arg *arg)
 {
 	Client *c = selmon->sel;
+	Client *o = c;
 
 	if (!selmon->lt[selmon->sellt]->arrange
 	|| (selmon->sel && selmon->sel->isfloating))
@@ -3128,6 +3129,7 @@ zoom(const Arg *arg)
 		if (!c || !(c = nexttiled(c->next)))
 			return;
 	pop(c);
+	focus(o);
 }
 
 int
