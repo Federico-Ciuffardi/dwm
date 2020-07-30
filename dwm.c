d31e2a2d6357f16e83d8136e5f2c50710d913b0b
Federico Ciuffardi <federico.ciuffardi@outlook.com>
2020-07-30 00:15:46 -0300

    adds focusmaster

--- dwm.c
+++ dwm.c
@@ -301,6 +301,7 @@ static int xerror(Display *dpy, XErrorEvent *ee);
 static int xerrordummy(Display *dpy, XErrorEvent *ee);
 static int xerrorstart(Display *dpy, XErrorEvent *ee);
 static void zoom(const Arg *arg);
+static void focusmaster(const Arg *arg);
 static void autostart_exec(void);
 
 static pid_t getparentprocess(pid_t p);
@@ -3212,7 +3213,24 @@ zoom(const Arg *arg)
 			return;
 	pop(c);
 	focus(o);
-	restack(selmon);
+}
+
+void
+focusmaster(const Arg *arg)
+{
+	Client *c = selmon->sel;
+
+	if (!selmon->lt[selmon->sellt]->arrange
+	|| (selmon->sel && (selmon->sel->isfloating || selmon->sel->isfullscreen)))
+		return;
+	/*if (c == nexttiled(selmon->clients))
+		if (!c || !(c = nexttiled(c->next)))
+			return;*/
+
+	for (c = selmon->clients; c && !ISVISIBLE(c); c = c->next);
+	if (!c)
+		return;
+	focus(c);
 }
 
 int
