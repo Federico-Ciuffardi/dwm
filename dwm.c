452afade436297adb140e456f73b4eb6199743fc
Federico Ciuffardi <federico.ciuffardi@outlook.com>
2020-08-12 02:20:18 -0300

    movemouse alteration

--- dwm.c
+++ dwm.c
@@ -1673,7 +1673,7 @@ motionnotify(XEvent *e)
 void
 movemouse(const Arg *arg)
 {
-	int x, y, ocx, ocy, nx, ny,tofloat = 0;
+	int x, y, ocx, ocy, nx, ny,wasfloating , moved = 0;
 	Client *c;
 	Monitor *m;
 	XEvent ev;
@@ -1683,6 +1683,7 @@ movemouse(const Arg *arg)
 		return;
 	if (c->isfullscreen) /* no support moving fullscreen windows by mouse */
 		return;
+	wasfloating = c->isfloating;
 	restack(selmon);
 	ocx = c->x;
 	ocy = c->y;
@@ -1714,10 +1715,11 @@ movemouse(const Arg *arg)
 				ny = selmon->wy;
 			else if (abs((selmon->wy + selmon->wh) - (ny + HEIGHT(c))) < snap)
 				ny = selmon->wy + selmon->wh - HEIGHT(c);
-			if (!c->isfloating && selmon->lt[selmon->sellt]->arrange
+			if (selmon->lt[selmon->sellt]->arrange
 			&& (abs(nx - c->x) > snap || abs(ny - c->y) > snap)){
-				togglefloating(NULL);
-				tofloat = 1;	
+				moved = 1;	
+				if (!c->isfloating)
+					togglefloating(NULL);
 			} if (!selmon->lt[selmon->sellt]->arrange || c->isfloating)
 				resize(c, nx, ny, c->w, c->h, 1);
 			break;
@@ -1730,7 +1732,7 @@ movemouse(const Arg *arg)
 		focus(NULL);
 	} else if( arg-> i)
 		pop(c);
-	if( arg->i && tofloat )
+	if( arg->i && (wasfloating != moved))
 		togglefloating(NULL);
 
 }
