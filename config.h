65ad4a619b67b09c3f12b8abbb875a316f32c31e
Federico Ciuffardi <federico.ciuffardi@outlook.com>
2020-08-27 02:17:03 -0300

    tag with mouse only

--- config.h
+++ config.h
@@ -186,7 +186,7 @@ static Button buttons[] = {
 	{ ClkClientWin,         0,              Button9,        killclient,     {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            0,              Button8,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 	{ ClkTabBar,            0,              Button1,        focuswin,       {0} },
 	{ ClkTabBar,            0,              Button2,        killwin ,       {0} },
