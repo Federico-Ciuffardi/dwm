ee9a4c33d9c4ce68d9f23ba5b1332d9277020379
Federico Ciuffardi <federico.ciuffardi@outlook.com>
2020-11-30 15:18:44 -0300

    fix crash when fs on empty ws

--- dwm.c
+++ dwm.c
@@ -2542,8 +2542,10 @@ togglefloating(const Arg *arg)
 void
 togglefullscreen(const Arg *arg)
 {
-	setfullscreen(selmon->sel, !selmon->sel->isfullscreen);
-	arrange(selmon);
+  if (selmon->sel){
+    setfullscreen(selmon->sel, !selmon->sel->isfullscreen);
+    arrange(selmon);
+  }
 }
 void
 togglefullfullscreen(const Arg *arg)
