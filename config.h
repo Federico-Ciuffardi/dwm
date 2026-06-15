8b8402b563142038c7d8b5a9e874caf53d397929
Federico Ciuffardi <federico.ciuffardi@focus.uy>
2026-06-15 13:45:16 -0300

    Change slack class

--- config.h
+++ config.h
@@ -178,7 +178,7 @@ static const Rule rules[] = {
   { "whatsapp-nativefier-d40211"  , NULL     , NULL                , ~0   , 1          , CENTER          , NN         , 0          , 0         ,     3 , -1      },
   { "com.github.th_ch.youtube_music"               , NULL     , NULL                , ~0   , 1          , CENTER          , NN         , 0          , 0         ,     6 , -1      },
   { "Mattermost"                  , NULL     , NULL                , ~0   , 1          , CENTER          , NN         , 0          , 0         ,     7 , -1      },
-  { "Slack"                       , NULL     , NULL                , ~0   , 1          , CENTER          , NN         , 0          , 0         ,    10 , -1      },
+  { "slack"                       , NULL     , NULL                , ~0   , 1          , CENTER          , NN         , 0          , 0         ,    10 , -1      },
   { "Gather"                      , NULL     , NULL                , ~0   , 1          , CENTER          , NN         , 0          , 0         ,    11 , -1      },
 };
 
